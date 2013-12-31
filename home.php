ca3f1462c0c7e26a68a5bd60625e20e4a3ea1f67
Michael Bishop <miklb@miklb.com>
2013-12-31 00:10:06 -0500

    adding pagination. Fixes Issue #2.

--- home.php
+++ home.php
@@ -4,6 +4,10 @@
       		<?php foreach ( $posts as $post ) { ?>
 				<?php echo $theme->content($post); ?>
 			<?php }  ?>
+			<div class="pagination">
+				<span class="next"><?php echo $theme->next_page_link( _t('Older') ); ?></span>
+				<span class="previous"><?php echo $theme->prev_page_link( _t('Newer') ); ?></span>
+			</div>
     	</div>
 	</div>
   </body>
