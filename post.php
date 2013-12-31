8840d8e11e3d8d91d7a86f14bc404a6f02f25ddb
Michael Bishop <miklb@miklb.com>
2013-12-31 00:26:54 -0500

    Fixed bug in post date, now uses user option Date Format

--- post.php
+++ post.php
@@ -2,7 +2,7 @@
 
   		<h1><a href="<?php echo $content->permalink; ?>" title="<?php echo $content->title; ?>"><?php echo $content->title_out; ?></a></h1>
   
- 		<span class="post-date"><data itemprop="datePublished" value="<?php echo $post->pubdate_datetime; ?>"><?php echo $post->pubdate_out; ?></data></span>
+ 		<span class="post-date"><data itemprop="datePublished" value="<?php echo $post->pubdate_datetime; ?>"><?php echo $content->pubdate->format(Options::get('dateformat')); ?></data></span>
     <?php echo $content->content_out; ?>
          
 	</article>
