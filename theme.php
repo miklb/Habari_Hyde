ed28e481386934b8b4b56275786b538d95d5e4da
Michael Bishop <miklb@miklb.com>
2014-01-02 03:18:14 -0500

    commenting this more tag thing out for now, will revisit

--- theme.php
+++ theme.php
@@ -16,7 +16,7 @@
 			// Apply Format::tag_and_list() to post tags...
 			Format::apply( 'tag_and_list', 'post_tags_out' );
 			// Only uses the <!--more--> tag, with the 'more' as the link to full post
-			Format::apply_with_hook_params( 'more', 'post_content_out', 'more' );
+			//Format::apply_with_hook_params( 'more', 'post_content_out', 'more' );
 			// Creates an excerpt option. echo $post->content_excerpt;
 			Format::apply( 'autop', 'post_content_excerpt' );
 			Format::apply_with_hook_params( 'more', 'post_content_excerpt', 'more', 60, 1 );
