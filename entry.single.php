0539c24be83576743fb9454ade245f0cad85009e
Michael Bishop <miklb@miklb.com>
2014-01-02 15:22:21 -0500

    First pass at adding comment form. Now to style comment output. Related #1

--- entry.single.php
+++ entry.single.php
@@ -6,8 +6,25 @@
  		<span class="post-date"><data itemprop="datePublished" value="<?php echo $post->pubdate_datetime; ?>"><?php echo $post->pubdate_out; ?></data></span>
 
  		<?php echo $post->content_out; ?>
+ 		<?php echo $theme->area( 'post_footer' ); ?>
+
+ 		<div class="comments" itemprop="comment">
+			<h3 id="comments">Comments</h1>
+			<?php if($post->comments->moderated->count == 0): ?>
+				<p><?php _e('There are no comments on this post.'); ?></p>
+			<?php else: ?>
+				<?php foreach($content->comments->moderated->comments as $comment): ?>
+					<?php echo $theme->content($comment); ?>
+				<?php endforeach; ?>
+			<?php endif; ?>
+			<?php if($post->info->comments_disabled): ?>
+				<p><?php _e('Sorry, commenting on this post is disabled.'); ?>
+			<?php else: ?>
+			<?php $post->comment_form()->out(); ?>
+			<?php endif; ?>
+		</div>
     	</article>
-    	<?php echo $theme->area( 'post_footer' ); ?>
+
 	</div>
 	<?php echo $theme->footer(); ?>
   </body>
