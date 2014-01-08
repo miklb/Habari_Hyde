75f32121a1df8230b652776c9c92862725a73593
Michael Bishop <miklb@miklb.com>
2014-01-07 21:42:02 -0500

    adding a 404 page. Might be temporary

---
+++ 404.php
@@ -0,0 +1,35 @@
+<?php $theme->display ( 'header' ); ?>	
+	<div class="content container">
+		<div class="page">
+      		<div class="fourohfour">
+      			<code>
+      		<nobr>      .o     .oooo.         .o      ooooooooo.                                  <br />
+    .d88    d8P'`Y8b      .d88      `888   `Y88.                                <br />
+  .d'888   888    888   .d'888       888   .d88'  .oooo.    .oooooooo  .ooooo.  <br />
+.d'  888   888    888 .d'  888       888ooo88P'  `P  )88b  888' `88b  d88' `88b <br />
+88ooo888oo 888    888 88ooo888oo     888          .oP"888  888   888  888ooo888 <br />
+     888   `88b  d88'      888       888         d8(  888  `88bod8P'  888    .o <br />
+    o888o   `Y8bd8P'      o888o     o888o        `Y888""8o `8oooooo.  `Y8bod8P' <br />
+                                                           d"     YD            <br />
+                                                           "Y88888P'            <br />
+                                                                                </nobr>
+
+<nobr>ooooo      ooo               .      oooooooooooo                                         .o8  <br />
+`888b.     `8'             .o8      `888'     `8                                        "888  <br />
+ 8 `88b.    8   .ooooo.  .o888oo     888          .ooooo.  oooo  oooo  ooo. .oo.    .oooo888  <br />
+ 8   `88b.  8  d88' `88b   888       888oooo8    d88' `88b `888  `888  `888P"Y88b  d88' `888  <br />
+ 8     `88b.8  888   888   888       888    "    888   888  888   888   888   888  888   888  <br />
+ 8       `888  888   888   888 .     888         888   888  888   888   888   888  888   888  <br />
+o8o        `8  `Y8bod8P'   "888"    o888o        `Y8bod8P'  `V88V"V8P' o888o o888o `Y8bod88P" <br />
+                                                                                              <br />
+                                                                                              <br />
+                                                                                              </nobr>
+
+				</code>
+
+			</div>
+    	</div>
+	</div>
+	<?php echo $theme->footer(); ?>
+  </body>
+</html>
