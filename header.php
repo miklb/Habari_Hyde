d5354000443828c2ece976720b7d68953b92cb16
Michael Bishop <miklb@miklb.com>
2014-04-26 01:28:57 -0400

    No, really this time use get_url to allow child themes to use their own favicon

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
   <title><?php if($request->display_entry && isset($post)) { echo "{$post->title} - "; } ?><?php Options::out( 'title' ) ?></title>
 
   <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php Site::out_url( 'theme' ); ?>/assets/touch-icon-144-precomposed.png">
-  <link rel="shortcut icon" href="<?php Site::get_url( 'theme' ); ?>/assets/img/favicon.png">
+  <link rel="shortcut icon" href="<?php echo $theme->get_url( '/assets/img/favicon.png' ); ?>">
 
   <?php echo $theme->header(); ?>
 
