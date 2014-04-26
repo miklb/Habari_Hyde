c8a3910abde204dde10d919b0d2b885f153a5d41
Michael Bishop <miklb@miklb.com>
2014-04-26 00:24:26 -0400

    use get_url to load favicon so child themes can use their own

--- header.php
+++ header.php
@@ -5,12 +5,12 @@
   <link href="http://gmpg.org/xfn/11" rel="profile">
 
   <title><?php if($request->display_entry && isset($post)) { echo "{$post->title} - "; } ?><?php Options::out( 'title' ) ?></title>
-  
+
   <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php Site::out_url( 'theme' ); ?>/assets/touch-icon-144-precomposed.png">
-  <link rel="shortcut icon" href="<?php Site::out_url( 'theme' ); ?>/assets/img/favicon.png">
-  
+  <link rel="shortcut icon" href="<?php Site::get_url( 'theme' ); ?>/assets/img/favicon.png">
+
   <?php echo $theme->header(); ?>
-  
+
 </head>
 <body class="<?php echo $theme->body_class(); ?>">
   <header class="masthead">
