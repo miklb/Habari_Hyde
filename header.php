5332768d2410c61b62c4d6328b528082d35ea0b6
Michael Bishop <miklb@miklb.com>
2014-11-22 00:48:35 -0500

    set the viewport

--- header.php
+++ header.php
@@ -1,25 +1,26 @@
 <!DOCTYPE html>
-<html lang="<?php echo $locale; ?>">
+<html lang="<?php echo $locale;?>">
 <head>
-  <meta charset="<?php echo $charset; ?>">
+  <meta charset="<?php echo $charset;?>">
   <link href="http://gmpg.org/xfn/11" rel="profile">
 
-  <title><?php if($request->display_entry && isset($post)) { echo "{$post->title} - "; } ?><?php Options::out( 'title' ) ?></title>
+  <title><?php if ($request->display_entry && isset($post)) {echo "{$post->title} - ";}?><?php Options::out('title')?></title>
 
-  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php Site::out_url( 'theme' ); ?>/assets/touch-icon-144-precomposed.png">
-  <link rel="shortcut icon" href="<?php echo $theme->get_url( '/assets/img/favicon.png' ); ?>">
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php Site::out_url('theme');?>/assets/touch-icon-144-precomposed.png">
+  <link rel="shortcut icon" href="<?php echo $theme->get_url('/assets/img/favicon.png');?>">
 
-  <?php echo $theme->header(); ?>
-
+<?php echo $theme->header();?>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
-<body class="<?php echo $theme->body_class(); ?>">
+<body class="<?php echo $theme->body_class();?>">
   <header class="masthead">
       <div class="masthead-inner">
-        <h1><a href="<?php Site::out_url( 'habari'); ?>"><?php Options::out( 'title' ); ?></a></h1>
-        <p class="lead"><?php Options::out( 'tagline' ); ?></p>
+        <h1><a href="<?php Site::out_url('habari');?>"><?php Options::out('title');
+?></a></h1>
+        <p class="lead"><?php Options::out('tagline');?></p>
 
         <div class="colophon">
-          <?php echo $theme->area( 'colophon' ); ?>
-        </div>
+<?php echo $theme->area('colophon');?>
+</div>
       </div>
     </header>
