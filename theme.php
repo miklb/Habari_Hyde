c683aca2b5755f010f9b72625d8bb3148366cc61
Michael Bishop <miklb@miklb.com>
2014-11-22 00:14:46 -0500

    move around some functions to be more child theme friendly

--- theme.php
+++ theme.php
@@ -1,41 +1,39 @@
 <?php
 
-	class Hyde extends Theme {
+class Hyde extends Theme {
 
-			function action_template_header($theme) {
-				//Add Fonts
-				Stack::add('template_stylesheet', '//fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,400italic|Abril+Fatface', 'fonts' );
-				//Add CSS
-				Stack::add ('template_stylesheet', array($this->get_url( '/assets/css/style.css' )),'main', 'extras');
-			}
+	function action_template_header($theme) {
+		//Add Fonts
+		Stack::add('template_stylesheet', '//fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,400italic|Abril+Fatface', 'fonts');
+		//Add CSS
+		Stack::add('template_stylesheet', array($this->get_url('/assets/css/style.css')), 'main', 'extras');
+	}
 
-		public function action_init_theme ( $theme ) {
+	public function action_init_theme($theme) {
 
-			// Apply Format::autop() to comment content...
-			Format::apply( 'autop', 'comment_content_out' );
-			// Apply Format::tag_and_list() to post tags...
-			Format::apply( 'tag_and_list', 'post_tags_out' );
-			// Only uses the <!--more--> tag, with the 'more' as the link to full post
-			//Format::apply_with_hook_params( 'more', 'post_content_out', 'more' );
-			// Creates an excerpt option. echo $post->content_excerpt;
-			Format::apply( 'autop', 'post_content_excerpt' );
-			Format::apply_with_hook_params( 'more', 'post_content_excerpt', 'more', 60, 1 );
-			Format::apply( 'format_date', 'post_pubdate_datetime','{Y}-{m}-{d}' );
-			Format::apply( 'format_date', 'post_pubdate_out', '{F} {j}{S}, {Y}');
+		// Apply Format::autop() to comment content...
+		Format::apply('autop', 'comment_content_out');
+		// Apply Format::tag_and_list() to post tags...
+		Format::apply('tag_and_list', 'post_tags_out');
+		// Only uses the <!--more--> tag, with the 'more' as the link to full post
+		//Format::apply_with_hook_params( 'more', 'post_content_out', 'more' );
+		// Creates an excerpt option. echo $post->content_excerpt;
+		Format::apply('autop', 'post_content_excerpt');
+		Format::apply_with_hook_params('more', 'post_content_excerpt', 'more', 60, 1);
+		Format::apply('format_date', 'post_pubdate_datetime', '{Y}-{m}-{d}');
+		Format::apply('format_date', 'post_pubdate_out', '{F} {j}{S}, {Y}');
 
+	}
 
-		}
+	public function action_add_template_vars($theme) {
 
-		public function action_add_template_vars($theme) {
-
-		if( !$theme->template_engine->assigned( 'pages' ) ) {
-					$theme->assign('pages',
-						Posts::get( array( 'content_type' => 'page',
-						    'status' => Post::status('published'),
-						    'nolimit' => 1 ) ) );
+		if (!$theme->template_engine->assigned('pages')) {
+			$theme->assign('pages',
+				Posts::get(array('content_type' => 'page',
+						'status'                      => Post::status('published'),
+						'nolimit'                     => 1)));
 		}
 	}
 
-
-	}
+}
 ?>
